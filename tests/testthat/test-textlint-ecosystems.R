19dee53fb0506a527778ccee33212dc5ab9c9fbf
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2018-10-04 05:25:27 +0900

    🔬 Test: textlint has been installed

--- tests/testthat/test-textlint-ecosystems.R
+++ tests/testthat/test-textlint-ecosystems.R
@@ -32,3 +32,39 @@ test_that("We enable modify a .textlintrc", {
       )
     })
 })
+
+test_that("Activate on textlint", {
+
+  withr::with_dir(
+    tempdir(), {
+      skip_if(dir.exists(".textlintr"))
+      expect_false(
+        dir.exists(".textlintr")
+      )
+      init_textlintr()
+      expect_true(
+        dir.exists(".textlintr")
+      )
+      expect_message(
+        init_textlintr(),
+        "Already, exits textlint.js"
+      )
+
+      textlint_res_raw <-
+        processx::run(
+          command = ".textlintr/node_modules/textlint/bin/textlint.js",
+          args = c("-f", "json",
+                   normalizePath(
+                     system.file("sample.md", package = "textlintr"))),
+          error_on_status = FALSE)
+      expect_is(
+        textlint_res_raw,
+        "list"
+      )
+      expect_length(
+        textlint_res_raw,
+        4
+      )
+    }
+  )
+})
