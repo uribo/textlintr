1ccde5ecddfe7f5f4f8a7f81728e79505c5a2689
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2018-10-03 23:54:20 +0900

    Separate by features

--- R/textlint.R
+++ R/textlint.R
@@ -3,28 +3,23 @@
 #' @param file filename whose target to textlint
 #' @param lintrc file path to .textlintrc. Default, searcing from current directory.
 #' @rdname textlint
+#' @examples
+#' \dontrun{
+#' lint_target <-
+#'   system.file("sample.md", package = "textlintr")
+#' textlint(lint_target)
+#' }
 #' @export
 textlint <- function(file = NULL, lintrc = ".textlintrc") {
-
-  if (rlang::is_false(file.exists(lintrc)))
-    rlang::abort("Missing .textlintrc")
 
   input_full_path <-
     normalizePath(file)
 
   lint_res <-
-    processx::run(command = "textlint",
-                  args = c("-f", "json", input_full_path),
-                  error_on_status = FALSE)
-
-  lint_res <-
-    gsub(
-      paste0(",\"filePath\":\"", input_full_path, "\"\\}\\]"),
-      "",
-      gsub("\\[\\{\"messages\":", "", lint_res$stdout))
+    lint_exec(file, lintrc)
 
   lint_res_parsed <-
-    jsonlite::fromJSON(lint_res)
+    lint_parse(lint_res)
 
   markers <-
     unname(apply(lint_res_parsed, 1, function(x) {
@@ -42,4 +37,32 @@ textlint <- function(file = NULL, lintrc = ".textlintrc") {
                       markers    = markers,
                       basePath   = NULL,
                       autoSelect = "first")
+}
+
+lint_exec <- function(file = NULL, lintrc = ".textlintrc") {
+  if (rlang::is_false(file.exists(lintrc)))
+    rlang::abort("Missing .textlintrc")
+
+  input_full_path <-
+    normalizePath(file)
+
+  lint_res <-
+    processx::run(command = "textlint",
+                  args = c("-f", "json", input_full_path),
+                  error_on_status = FALSE)
+
+  lint_res$input_full_path <-
+    input_full_path
+
+  lint_res
+}
+
+lint_parse <- function(lint_res) {
+  lint_res <-
+    gsub(
+      paste0(",\"filePath\":\"", lint_res$input_full_path, "\"\\}\\]"),
+      "",
+      gsub("\\[\\{\"messages\":", "", lint_res$stdout))
+
+  jsonlite::fromJSON(lint_res)
 }
