50b44dd853a015201768b22eb944b9d046973d1d
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2018-10-09 23:09:32 +0900

    Added to textlintrc configuration function

--- R/rules.R
+++ R/rules.R
@@ -1,3 +1,28 @@
+#' Config textlint rule file
+#'
+#' @inheritParams textlint
+#' @param open If you want to modify a `.textlintrc`, set `TRUE`. (deafult `FALSE`)
+#' @rdname textlint_rules
+#' @examples
+#' \dontrun{
+#' configure_lint_rules()
+#' }
+#' @return list
+#' @export
+configure_lint_rules <- function(lintrc = ".textlintrc", open = FALSE) {
+
+  check_rule_exist(lintrc)
+
+  if (rlang::is_true(open))
+    if (rlang::is_true(rstudioapi::isAvailable()))
+      rstudioapi::navigateToFile(lintrc)
+  else
+    utils::edit(lintrc)
+
+  jsonlite::read_json(lintrc)[["rules"]]
+
+}
+
 check_rule_exist <- function(lintrc = ".textlintrc") {
   if (rlang::is_false(file.exists(lintrc)))
     rlang::abort(
---
+++ tests/testthat/test-rules.R
@@ -0,0 +1,30 @@
+context("test-rules")
+
+test_that(".textlintrc works", {
+
+  expect_error(
+    check_rule_exist()
+  )
+  expect_error(
+    configure_lint_rules(lintrc = "textlintrc"),
+    "Missing .textlintrc."
+  )
+
+  withr::with_dir(
+    tempdir(), {
+      update_lint_rules(rules = c("common-misspellings",
+                                  "preset-jtf-style"))
+      res <-
+        configure_lint_rules(".textlintrc", open = FALSE)
+      expect_is(res, "list")
+      expect_length(res, 2)
+      expect_named(res, c("common-misspellings",
+                          "preset-jtf-style"))
+      update_lint_rules(rules = c("preset-ja-technical-writing"))
+      res <-
+        configure_lint_rules()
+      expect_named(res, c("preset-ja-technical-writing"))
+      expect_true(res[[1]])
+    }
+  )
+})
